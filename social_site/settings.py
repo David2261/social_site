ef7aa56cc4f85c24a4fe359dd2df69d850671195
David2261 <bulatnasirov2003@gmail.com>
2022-01-15 01:39:44 +0300

    finally correct

--- social_site/settings.py
+++ social_site/settings.py
@@ -38,12 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Apps
+    'rest_framework',
+    "corsheaders",
+
     "base.apps.BaseConfig",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +130,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+CORS_ALLOWED_ORIGINS = True
